e51305fcbddcf61f0f89e65cb2ec5110e267a1d4
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-12 00:43:26 +0530

    commit

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,9 @@
-import {PrimaClient} from "../generated/prisma/index.js"
+import { PrimaClient } from "../generated/prisma/index.js"
 
 const globalForPrisma = globalThis;
 
-export const db = globalForPrisma.prisma || new PrimaClient();
+export const db = globalForPrisma.prisma || new PrimaClient({
+    log: ["query", "info", "warn", "error"],
+});
 
-if(process.env.NODE_ENV !== "production") globalForPrisma.prisma = db ;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
