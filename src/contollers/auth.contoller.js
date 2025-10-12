1fcd3c1ae6a1749c930cdbcd8091ad93526557d3
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-12 08:38:04 +0530

    commit

--- src/contollers/auth.contoller.js
+++ src/contollers/auth.contoller.js
@@ -5,7 +5,37 @@ import bcrypt from "bcryptjs";
 import {db} from "../lib/db.js";
 import { UserRole } from "../generated/prisma/index.js";
 
-export const register = async (req , res)=>{}
+export const register = async (req , res)=>{
+    const {email, password, name} = req.body;
+
+    try {
+        const existingUser = await db.user.findUnique({
+            where:{email}
+        })
+
+        if(existingUser){
+            return res.status(400).json({
+                error:"User already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await db.user.create({
+            data:{
+                email,
+                password:hashedPassword,
+                name,
+                role:UserRole.USER
+            }
+        })
+
+        const token = jwt.sign({id:newUser.id});
+
+    } catch (error) {
+        
+    }
+}
 
 
 export const login = async (req , res)=>{}
