ef0e23c09b75e4cd46469fbc08b41d309ebf89fd
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-12 08:20:09 +0530

    commit

---
+++ src/contollers/auth.contoller.js
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+
+import {db} from "../lib/db.js";
+import { UserRole } from "../generated/prisma/index.js";
+
+export const register = async (req , res)=>{}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { register } from "../contollers/auth.contoller.js";
 
 const router = express.Router();
 
 // 1.register
 
-router.post('/register',register42)
+router.post('/register',register)
 // 2.login
 // 3.logout
 // 4.check
